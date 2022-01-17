9b74f2aeb6b0de440e4813008f0084571b42b85e
Dhruv Reddy <dhruv.reddy700@gmail.com>
2022-01-17 11:49:42 -0800

    Ui finished

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,8 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            ZStack{
+                Image("background")
+                    .resizable()
+                    .ignoresSafeArea()
+                VStack{
+                    Image("logo")
+                        .resizable()
+                        .frame(width: geo.size.width / 2, height: geo.size.height / 8, alignment: .center)
+                    Spacer()
+                    HStack{
+                        Spacer()
+                        Image("card2")
+                        Spacer()
+                        Image("card3")
+                        Spacer()
+                    }
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Image("dealbutton")
+                            .resizable()
+                            .frame(width: geo.size.width / 2, height: geo.size.height / 10, alignment: .center)
+                    }
+                    Spacer()
+                    HStack{
+                        Spacer()
+                        VStack{
+                            Text("Player")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                                .padding(1)
+                            Text("0")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                                .padding(1)
+                        }
+                        Spacer()
+                        VStack{
+                            Text("Computer")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                                .padding(1)
+                            Text("0")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                                .padding(1)
+                        }
+                        Spacer()
+                    }
+
+                }
+            }
+        }
     }
 }
 
